cce68d28587a6512f803c2737678b2bbb9ea2530
eduardohufg <eduardochavezmartin10@gmail.com>
2024-04-17 23:56:48 -0600

    initial login

---
+++ fornt_app/src/main.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './routes/Login.tsx';
+import Signup from './routes/signup.tsx';
+import Dashboard from './routes/Dashboard.tsx';
+import Protected from './routes/Protected.tsx';
+import { AuthProvider } from './auth/AuthProvider.tsx';
+
+const router = createBrowserRouter([
+  
+  { path: "/", 
+  element: <Login />,
+ },
+ {
+  path: "/signup",
+  element: <Signup />,
+ },
+ {
+  path: "/",
+  element: <Protected />,
+  children: [
+    { path: "/dashboard", element: <Dashboard /> },
+  ],
+ }
+
+ 
+
+
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router}/>
+    </AuthProvider>
+  </React.StrictMode>,
+)
