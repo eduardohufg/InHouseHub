3d697b4f26723ee9fd6a33e384524b09f5e5f923
Eduardo Chavez Martin <66572435+eduardohufg@users.noreply.github.com>
2024-05-07 19:37:01 -0600

    server implementation

--- backend/internal/server.go
+++ backend/internal/server.go
@@ -1,1 +1,17 @@
 package internal
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func StartServer() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":5000"))
+}
